0a45718bc8c960a3bc3ac8889adfd5bb14e727ed
Younglion6 <jonathan_drakes@hotmail.com>
2023-11-30 16:01:36 -0400

    ##

--- crm/settings.py
+++ crm/settings.py
@@ -34,7 +34,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'jazzmin'
+    'jazzmin',
     'webapp',
     'crispy_bootstrap4',
     'crispy_forms',
